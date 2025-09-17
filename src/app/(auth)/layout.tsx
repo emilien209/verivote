cd8b64953f63c51c131df98f209ba7ba8ce8f539
ndishimye emilien <ndishimyeemilien29@gmail.com>
2025-09-17 05:37:26 +0000

    1. Design Style & Visual Identity
    Clean, Official Look
    
    Colors: Use Rwa

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,4 @@
 import { Logo } from '@/components/logo';
-import Image from 'next/image';
 
 export default function AuthLayout({
   children,
@@ -7,27 +6,15 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2 xl:min-h-screen">
-      <div className="flex items-center justify-center py-12">
-        <div className="mx-auto grid w-[350px] gap-6">
-           <div className="grid gap-2 text-center">
+    <div className="flex items-center justify-center min-h-screen bg-background">
+        <div className="mx-auto grid w-[380px] gap-6 px-4">
+           <div className="grid gap-4 text-center">
              <div className="flex justify-center items-center gap-2 mb-4">
                 <Logo />
             </div>
             {children}
            </div>
         </div>
-      </div>
-      <div className="hidden bg-muted lg:block">
-        <Image
-          src="https://picsum.photos/seed/picsum/1920/1080"
-          alt="Image"
-          width="1920"
-          height="1080"
-          data-ai-hint="voting democracy"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
-      </div>
     </div>
   );
 }
