55e9cb50c318e3ba080e191284e5c4cad8f3920b
ndishimye emilien <ndishimyeemilien29@gmail.com>
2025-09-18 07:40:30 +0000

    use this fire baase{// Import the functions you need from the SDKs you n

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAN95KDrL7_VbM6DA5jnanYYHXZLLDi6NQ",
-  authDomain: "tora-b66a1.firebaseapp.com",
-  projectId: "tora-b66a1",
-  storageBucket: "tora-b66a1.appspot.com",
-  messagingSenderId: "474621598483",
-  appId: "1:474621598483:web:864347e5d5e570d179d8e6",
-  measurementId: "G-GX4G4LNMVG"
+  apiKey: "AIzaSyAQ3va2TXWkWli1j4fHiO_DuPDlzSth3FY",
+  authDomain: "verivot-23d3b.firebaseapp.com",
+  projectId: "verivot-23d3b",
+  storageBucket: "verivot-23d3b.appspot.com",
+  messagingSenderId: "113048594555",
+  appId: "1:113048594555:web:8937fb2cb8029f0eb95ade",
+  measurementId: "G-K8H54XY3Q9"
 };
 
 
