3434fac03cb1b26a933fc16ada9d4d9b7aaad357
ndishimye emilien <ndishimyeemilien29@gmail.com>
2025-09-20 08:16:59 +0000

    use da in fire base

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAQ3va2TXWkWli1j4fHiO_DuPDlzSth3FY",
   authDomain: "verivot-23d3b.firebaseapp.com",
   projectId: "verivot-23d3b",
-  storageBucket: "verivot-23d3b.appspot.com",
+  storageBucket: "verivot-23d3b.firebasestorage.app",
   messagingSenderId: "113048594555",
   appId: "1:113048594555:web:8937fb2cb8029f0eb95ade",
   measurementId: "G-K8H54XY3Q9"
